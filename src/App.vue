a95d6e837c14aeb6def4b6a6975e422c64ef1b99
Nucci-karim <nuccikarim@gmail.com>
2023-05-07 16:32:38 +0200

    inizio

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import HeaderComp from './components/HeaderComp.vue'
+
+export default{
+  name: 'app',
+  components:{
+    HeaderComp,
+  }
+}
+</script>
+
+<template>
+  <HeaderComp/>
+</template>
+
+<style lang="scss">
+@use './style/main.scss'
+
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+  name: 'HeaderComp',
+  
+}
+</script>
+
+<template>
+  <div class="container d-flex align-items-center">
+    <img src="" alt="logo">
+    <h1 class="mx-2">Yu-Gi-Oh API</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
